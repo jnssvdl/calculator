bbb7b31cd1deec5ef0e58b769f7ad8f8de404b1d
Joeniesus Vidal <joeniesusvidal01@gmail.com>
2023-08-08 16:20:45 +0800

    Fixed calculate functionality

--- main.js
+++ main.js
@@ -14,8 +14,17 @@ const divide = function (x, y) {
     return x / y;
 };
 
-const operate = function (operator, x, y) {
-
+const operate = function (o, x, y) {
+    x = Number(x), y = Number(y);
+    if (o === '+') {
+        return add(x, y);
+    } else if (o === '-') {
+        return subtract(x, y);
+    } else if (o === '*') {
+        return multiply(x, y);
+    } else if (o ===  '/') {
+        return divide(x, y);
+    }
 };
 
 
@@ -28,6 +37,25 @@ const display = document.getElementById('display');
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        display.textContent += number.id;
+        if (operator === '') {
+            firstNumber += number.id;
+        } else {
+            secondNumber += number.id;
+        }
     });
+});
+
+const operations = document.querySelectorAll('.operation');
+operations.forEach((operation) => {
+    operation.addEventListener('click', () => {
+        operator = operation.id;
+    });
+}); 
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => {
+    let result = operate(operator, firstNumber, secondNumber);
+    console.log(result);
+    firstNumber = result;
+    secondNumber = '';
 });
